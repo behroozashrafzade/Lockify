ae744a6420ef54f6412880f3dfbab3ae38194917
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-08-08 22:31:31 +0330

    add class PasswordEntry and  property

---
+++ Core/Models/PasswordEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class PasswordEntry
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+ 
+        public required string Email { get; set; }
+
+        public required string PasswordHash { get; set; }
+
+        public required string Website { get; set; }
+
+
+        public required DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
