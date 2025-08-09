4e5483f83fa962d208d6ac35e0c755a4de8c8d76
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-08-09 15:44:51 +0330

    add interface ICloudService and Implementation

---
+++ Application/Interfaces/ICloudService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ICloudService
+    {
+        /// <summary>
+        /// احراز هویت کاربر در سرویس ابری
+        /// </summary>
+        Task<bool> AuthenticateAsync();
+
+        /// <summary>
+        /// آپلود فایل پسوردها به سرویس ابری
+        /// </summary>
+        Task UploadFileAsync(string localFilePath, string remoteFileName);
+
+        /// <summary>
+        /// دانلود فایل پسوردها از سرویس ابری
+        /// </summary>
+        Task DownloadFileAsync(string remoteFileName, string localFilePath);
+
+        /// <summary>
+        /// بررسی اینکه آیا فایل روی سرویس ابری وجود دارد یا نه
+        /// </summary>
+        Task<bool> FileExistsAsync(string remoteFileName);
+    
+    }
+}
