9e4807fa0dcac2416abf48ec98335175e945dad1
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-08-14 17:57:47 +0330

    add BaseViewModel and LoginViewModel

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lockify.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set => SetProperty(ref _email, value);
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLogin);
+        }
+
+        private void OnLogin()
+        {
+            // TODO: اتصال به سرویس لاگین
+        }
+    }
+}
